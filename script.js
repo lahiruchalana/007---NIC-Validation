ebddfcd1b1ec6f1e446fb059359f239f6cefa695
W.D. Lahiru Chalana Iresh <55834083+lahiruchalana@users.noreply.github.com>
2021-08-11 16:21:30 +0530

    first commit

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const nic = document.getElementById('NIC')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
 const nicno = document.getElementById('NICno')
+const type = document.getElementById('type')
 
 form.addEventListener('submit', (e) => {
+    // add errors to messages for NIC number input
     let messages = []
     if (nic.value === '' || nic.value == null) {
         messages.push('NIC number is required')
@@ -13,11 +15,22 @@ form.addEventListener('submit', (e) => {
         errorElement.innerText = messages.join(', ')
     }
 
-    if (nic.value.length === 10 || nic.value.length === 12) {
-        nicno.innerHTML = "Your NIC number is: "+ nic.value
-    }
-
-    if (!nic.value.length === 10 || nic.value.length === 12) {
+    // identify old valid NIC 
+    if (nic.value.length === 10) {
+        if (nic.value.substring( nic.value.length - 1) == 'v' || 
+            nic.value.substring( nic.value.length - 1) == 'x' ||
+            nic.value.substring( nic.value.length - 1) == 'V' ||
+            nic.value.substring( nic.value.length - 1) == 'X' ) {
+            nicno.innerHTML = "Your NIC number is: " + nic.value
+            type.innerHTML = "This is an old NIC"            
+        } else {
+            nicno.innerHTML = nic.value + " has a invalid last character"
+        }
+    } else {
+        // This should be chaged with last charater
+        // because when some one put 11 numbers with a letter then nic.value.length === 12 , but this is not a valid NIC
+        // so change this code with last character 
+        // add comments to understand the code 
         nicno.innerHTML = nic.value + " is not a valid NIC number"
     }
 
