ddea8be96d2a99844152ca00fd0f33be6d6838d9
W.D. Lahiru Chalana Iresh <55834083+lahiruchalana@users.noreply.github.com>
2021-08-12 22:08:22 +0530

    fixed bugs on substring -> substr

--- script.js
+++ script.js
@@ -27,13 +27,15 @@ form.addEventListener('submit', (e) => {
             nic.value.substring( nic.value.length - 1) == 'V' ||
             nic.value.substring( nic.value.length - 1) == 'X' ) {
                 // find the gender and dob months and dates 
+                if (nic.value.substr(2, 3) === '000') {
+                    nicno.innerHTML = nic.value + " is not a valid NIC number"
+                } 
                 // (if 3+4+5 numbers are < 366 --> male)
-                if (nic.value.substring(2, 3) <= '366') {
+                else if (nic.value.substring(2, 3) <= '366') {
                     nicno.innerHTML = "Your NIC number is: " + nic.value
                     type.innerHTML = "This is an old NIC"
-                    // find out days converting to months and dates
                     dob.innerHTML = "Date of Birth: " + nic.value.substring(0, 2) + "/" 
-                    age.innerHTML = "Age: "
+                    age.innerHTML = "Age: " + (2021 - (19 + nic.value.substring(0, 2)))
                     gender.innerHTML = "Gender: Male" 
                 }
                 // (if 366 < 3+4+5 numbers are < 500 --> not a valid NIC)
@@ -44,9 +46,8 @@ form.addEventListener('submit', (e) => {
                 else if ('500' <= nic.value.substring(2, 3) && nic.value.substring(2, 3) <= '866') {
                     nicno.innerHTML = "Your NIC number is: " + nic.value
                     type.innerHTML = "This is an old NIC"
-                    // find out days converting to months and dates
                     dob.innerHTML = "Date of Birth: " + nic.value.substring(0, 2) + "/" 
-                    age.innerHTML = "Age: "
+                    age.innerHTML = "Age: " + (2021 - (19 + nic.value.substring(0, 2)))
                     gender.innerHTML = "Gender: Female" 
                 }
                 // (if 866 < 3+4+5 numbers are < 999 --> female)
@@ -59,33 +60,37 @@ form.addEventListener('submit', (e) => {
     } 
     // identify new valid NIC 
     else if (nic.value.length === 12) {
-        // find the gender and dob months and dates 
+        // find the gender and dob months and dates
+        if (nic.value.substr(4, 3) === '000') {
+            nicno.innerHTML = nic.value + " is not a valid NIC number"
+        } 
         // (if 3+4+5 numbers are < 366 --> male)
-        if (nic.value.substring(4, 3) <= 366) {
+        else if (nic.value.substr(4, 3) <= '366') {
             nicno.innerHTML = "Your NIC number is: " + nic.value
             type.innerHTML = "This is a new NIC"
-            // find out days converting to months and dates
             dob.innerHTML = "Date of Birth: " + nic.value.substring(0, 4) + "/" 
-            age.innerHTML = "Age: "
+            age.innerHTML = "Age: " + (2021 - nic.value.substring(0, 4))
             gender.innerHTML = "Gender: Male" 
         }
         // (if 366 < 3+4+5 numbers are < 500 --> not a valid NIC)
-        else if (366 < nic.value.substring(4, 3) < 500) {
+        else if ('366' < nic.value.substr(4, 3) && nic.value.substr(4, 3) < '500') {
             nicno.innerHTML = nic.value + " is not a valid NIC number"
         }
         // (if 500 <= 3+4+5 numbers are <= 866 --> female)
-        else if (500 <= nic.value.substring(4, 3) <= 866) {
+        else if ('500' <= nic.value.substr(4, 3) && nic.value.substr(4, 3) <= '866') {
             nicno.innerHTML = "Your NIC number is: " + nic.value
             type.innerHTML = "This is a new NIC"
-            // find out days converting to months and dates
             dob.innerHTML = "Date of Birth: " + nic.value.substring(0, 4) + "/" 
-            age.innerHTML = "Age: "
+            age.innerHTML = "Age: " + (2021 - nic.value.substring(0, 4))
             gender.innerHTML = "Gender: Female" 
         }
         // (if 866 < 3+4+5 numbers are < 999 --> female)
-        else if (866 < nic.value.substring(4, 3) < 999) {
+        else if ('866' < nic.value.substr(4, 3) && nic.value.substr(4, 3) < '999') {
             nicno.innerHTML = nic.value + " is not a valid NIC number"
         }            
+    } 
+    else if (nic.value.length === 0) {
+        nicno.innerHTML = "Please add your NIC number"
     } else {
         nicno.innerHTML = nic.value + " is not a valid NIC number"
     }
