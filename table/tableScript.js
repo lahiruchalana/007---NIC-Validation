f5f9c1363be221ac022271bf40aae6112e1f8ca4
W.D. Lahiru Chalana Iresh <55834083+lahiruchalana@users.noreply.github.com>
2021-08-20 10:08:46 +0530

    added old and new NIC number searching in tableScript

--- table/tableScript.js
+++ table/tableScript.js
@@ -1,10 +1,10 @@
 var myArray = [
-    {'name':'Lahiru', 'oldNIC':'971811102v', 'newNIC':'199718102548', 'age':'30', 'birthdate':'11/10/1989'},
-    {'name':'Isuru', 'oldNIC':'952818988v', 'newNIC':'199528108988', 'age':'32', 'birthdate':'10/1/1989'},
-    {'name':'Kasun', 'oldNIC':'971512256x', 'newNIC':'199715102256', 'age':'29', 'birthdate':'10/14/1990'},
-    {'name':'Shanuka', 'oldNIC':'981916243v', 'newNIC':'199819156243', 'age':'25', 'birthdate':'11/29/1993'},
-    {'name':'Amila', 'oldNIC':'990815621v', 'newNIC':'199908195621', 'age':'27', 'birthdate':'3/12/1991'},
-    {'name':'Madushan', 'oldNIC':'002515412v', 'newNIC':'200025145412', 'age':'24', 'birthdate':'10/31/1995'},
+    {'name':'Lahiru', 'NIC':'971811102v', 'referralNIC':'', 'age':'30', 'birthdate':'11/10/1989'},
+    {'name':'Isuru', 'NIC':'199528108988', 'referralNIC':'952818988v', 'age':'32', 'birthdate':'10/1/1989'},
+    {'name':'Kasun', 'NIC':'971512256x', 'referralNIC':'', 'age':'29', 'birthdate':'10/14/1990'},
+    {'name':'Shanuka', 'NIC':'199819156243', 'referralNIC':'981916243v', 'age':'25', 'birthdate':'11/29/1993'},
+    {'name':'Amila', 'NIC':'990815621v', 'referralNIC':'', 'age':'27', 'birthdate':'3/12/1991'},
+    {'name':'Madushan', 'NIC':'200025145412', 'referralNIC':'002515412v', 'age':'24', 'birthdate':'10/31/1995'},
 ]
 
 $('#search-input').on('keyup', function(){
@@ -21,54 +21,49 @@ function searchTable(value, data) {
     for (var i = 0; i < data.length; i++) {
         value = value.toLowerCase()
         var name = data[i].name.toLowerCase()
-        var oldNIC = data[i].oldNIC
-        var newNIC = data[i].newNIC
+        var NIC = data[i].NIC
+        // filter by name
         if (name.includes(value)) {
             filteredData.push(data[i])
-        } else if (oldNIC.includes(value)) {
-            filteredData.push(data[i])
-        } else if (newNIC.includes(value)) {
-            filteredData.push(data[i])
+        } else if (value.length === 9) { // old NIC filter without v/x
+            if (NIC.includes(value)) {
+                filteredData.push(data[i])
+            } 
+        } else if (value.length === 10) { // old NIC filter
+            if (NIC.includes(value)) {
+                filteredData.push(data[i])
+            } 
+        } else if (value.length === 12) { // new NIC filter
+            var birthYearDOB = NIC.substr(0, 5)
+            var valueBirthYearDOB = value.substr(2, 5)
+            var serialCode = NIC.substr(5, 4)
+            var valueSerialCode = value.substr(8, 4)
+            if (birthYearDOB.includes(valueBirthYearDOB)) {
+                if (serialCode.includes(valueSerialCode)) {
+                    filteredData.push(data[i])
+                }
+            } else if (NIC.includes(value)) {
+                filteredData.push(data[i])
+            } 
         }
     }
     return filteredData
 }
-
-$('th').on('click', function(){
-    var column = $(this).data('colname')
-    var order = $(this).data('order')
-    var text = $(this).html()
-    text = text.substring(0, text.length - 1);
-    
-    if (order == 'desc'){
-        myArray = myArray.sort((a, b) => a[column] > b[column] ? 1 : -1)
-        $(this).data("order","asc");
-        text += '&#9660'
-    }else{
-        myArray = myArray.sort((a, b) => a[column] < b[column] ? 1 : -1)
-        $(this).data("order","desc");
-        text += '&#9650'
-    }
-
-    $(this).html(text)
-    buildTable(myArray)
-    })
-
 
 function buildTable(data){
     var table = document.getElementById('myTable')
     table.innerHTML = ''
     for (var i = 0; i < data.length; i++){
         var colname = `name-${i}`
-        var cololdNIC = `oldNIC-${i}`
-        var colnewNIC = `newNIC-${i}`
+        var colNIC = `NIC-${i}`
+        var colreferralNIC = `referralNIC-${i}`
         var colage = `age-${i}`
         var colbirth = `birth-${i}`
 
         var row = `<tr>
                         <td>${data[i].name}</td>
-                        <td>${data[i].oldNIC}</td>
-                        <td>${data[i].newNIC}</td>
+                        <td>${data[i].NIC}</td>
+                        <td>${data[i].referralNIC}</td>
                         <td>${data[i].age}</td>
                         <td>${data[i].birthdate}</td>
                     </tr>`
