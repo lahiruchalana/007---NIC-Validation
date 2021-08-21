2df018e72fbbb04091f820773f674b50cc2d5789
W.D. Lahiru Chalana Iresh <55834083+lahiruchalana@users.noreply.github.com>
2021-08-21 06:42:47 +0530

    new NIC table filtering error fixed

--- table/tableScript.js
+++ table/tableScript.js
@@ -3,15 +3,15 @@ const referralButton = document.getElementById("referralButton")
 var update = document.getElementById('referralButton')
 
 var myArray = [
-    {'name':'Lahiru', 'NIC':'971811102v', 'referralNIC':'6256256', 'age':'30', 'birthdate':'11/10/1989'},
+    {'name':'Lahiru', 'NIC':'971811102v', 'referralNIC':'', 'age':'30', 'birthdate':'11/10/1989'},
     {'name':'Isuru', 'NIC':'199528108988', 'referralNIC':'952818988v', 'age':'32', 'birthdate':'10/1/1989'},
-    {'name':'Kasun', 'NIC':'971512256x', 'referralNIC':'165', 'age':'29', 'birthdate':'10/14/1990'},
+    {'name':'Kasun', 'NIC':'971512256x', 'referralNIC':'', 'age':'29', 'birthdate':'10/14/1990'},
     {'name':'Shanuka', 'NIC':'199819156243', 'referralNIC':'981916243v', 'age':'25', 'birthdate':'11/29/1993'},
-    {'name':'Amila', 'NIC':'990815621v', 'referralNIC':'52626', 'age':'27', 'birthdate':'3/12/1991'},
+    {'name':'Amila', 'NIC':'990815621v', 'referralNIC':'', 'age':'27', 'birthdate':'3/12/1991'},
     {'name':'Madushan', 'NIC':'200025145412', 'referralNIC':'002515412v', 'age':'24', 'birthdate':'10/31/1995'},
-    {'name':'Shashini', 'NIC':'	962916243x', 'referralNIC':'1454', 'age':'24', 'birthdate':'10/31/1995'},
+    {'name':'Shashini', 'NIC':'	962916243x', 'referralNIC':'', 'age':'24', 'birthdate':'10/31/1995'},
     {'name':'Iresh', 'NIC':'200135145412', 'referralNIC':'013515412v', 'age':'24', 'birthdate':'10/31/1995'},
-    {'name':'Chalana', 'NIC':'	8129106243v', 'referralNIC':'87418751', 'age':'24', 'birthdate':'10/31/1995'},
+    {'name':'Chalana', 'NIC':'	8129106243v', 'referralNIC':'', 'age':'24', 'birthdate':'10/31/1995'},
     {'name':'Padmila', 'NIC':'200225145412', 'referralNIC':'022515412v', 'age':'24', 'birthdate':'10/31/1995'},
 ]
 
@@ -48,12 +48,16 @@ function searchTable(value, data) {
             var valueBirthYearDOB = value.substr(2, 5)
             var serialCode = NIC.substr(5, 4)
             var valueSerialCode = value.substr(8, 4)
-            if (birthYearDOB.includes(valueBirthYearDOB)) {
+            if (NIC.includes(value)) {
+                filteredData.push(data[i])
+                attention.innerHTML = "Your NIC " + NIC + " is Available"
+            }
+            else if (birthYearDOB.includes(valueBirthYearDOB)) {
                 if (serialCode.includes(valueSerialCode)) {
                     filteredData.push(data[i])
                     attention.innerHTML = "Your Old NIC " + NIC + " is Available"
                     referralButton.innerHTML = "Update Your Old NIC to New NIC"
-                    update.onclick = function() {
+                    update.onclick = function() { // append new NIC value
                         var name = filteredData[0].name
                         var age = filteredData[0].age
                         var birthdate = filteredData[0].birthdate
